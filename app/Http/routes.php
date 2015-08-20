e13413b52c2ab52b5f3dcc9aef69465cac09b629
Timothy Teoh <chalcedonyt@gmail.com>
2015-08-21 02:24:51 +0800

    11 - Add basic routes and controllers for Club, Activity, and User.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::get('club',              ['as' => 'club-index', 'uses' => 'ClubController@index']);
+    Route::get('club/{id}',         ['as' => 'club-show', 'uses' => 'ClubController@show']);
+    Route::get('activity',          ['as' => 'activity-index', 'uses' => 'ActivityController@index']);
+    Route::get('activity/{id}',     ['as' => 'activity-show', 'uses' => 'ActivityController@show']);
+    Route::get('user',              ['as' => 'user-index', 'uses' => 'UserController@index']);
+    Route::get('user/{id}',         ['as' => 'user-show', 'uses' => 'UserController@show']);
+});
