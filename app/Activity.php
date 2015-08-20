78e485ea9f4a7844acc17fefb3fe35285ca2e2ea
Timothy Teoh <chalcedonyt@gmail.com>
2015-08-21 02:23:14 +0800

    10 - Add relations to Club and Activity

--- app/Activity.php
+++ app/Activity.php
@@ -8,4 +8,12 @@ class Activity extends Model
 {
     protected $table = 'activities';
     protected $guarded = [];
+
+    public function club(){
+        return $this -> belongsTo('App\Club');
+    }
+
+    public function participants(){
+        return $this -> belongsToMany('App\User','activity_participants');
+    }
 }
--- app/Club.php
+++ app/Club.php
@@ -8,4 +8,16 @@ class Club extends Model
 {
     protected $table = 'clubs';
     protected $guarded = [];
+
+    public function members(){
+        return $this -> belongsToMany('App\User','club_members');
+    }
+
+    public function admins(){
+        return $this -> belongsToMany('App\User','club_admins');
+    }
+
+    public function activities(){
+        return $this -> hasMany('App\Activity');
+    }
 }
